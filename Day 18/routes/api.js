535d276a50b72447cdb5cce50f6064fe46f71103
Nicole <nholt3@atu.edu>
2025-03-19 13:46:11 -0500

    added routes

---
+++ Day 18/routes/api.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const authMiddleware = require('../middlewares/authMiddleware');
+
+const router = express.Router();
+
+router.get('/public', (req, res) => {
+    res.send({message: 'Welcome to the Public API!'});
+});
+
+router.get('/protected', authMiddleware, (req, res) => {
+    res.send({message: 'Welcome to the Protected API!'});
+});
+
+router.get('/error', (req,res,next) => {
+    const error = new Error('Something went wrong!');
+    next(error);
+})
+
+module.exports = router;
